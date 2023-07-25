afa947a81af608699bd21d3e6b16933d0a9d8f56
bhavini-16 <bhavinikorthi16@gmail.com>
2023-07-25 18:34:30 +0530

    added database name

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-conn = st.experimental_connection('pet_db', type='sql')
+conn = st.experimental_connection('streamlit', type='sql')
 students = conn.query('select * from student')
 st.dataframe(students)
